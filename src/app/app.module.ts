42b528596f9189b3533d06b0d2dc987e3696b1d8
ONBASE\cross <chris.ross@onbase.net>
2018-03-05 11:55:47 -0500

    Hooked up and styled the nav bar. Also, created the shell of a new user page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { PersonService } from './shared/person.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PersonListComponent } from './person-list/person-list.component';
 import { PersonComponent } from './person/person.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NewUserComponent } from './new-user/new-user.component';
+
 
 
 @NgModule({
@@ -14,10 +17,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     AppComponent,
     PersonListComponent,
     PersonComponent,
-    NavBarComponent
+    NavBarComponent,
+    NewUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [PersonService],
   bootstrap: [AppComponent]
