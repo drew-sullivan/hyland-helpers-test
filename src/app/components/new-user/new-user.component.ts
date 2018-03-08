4d6dbfd468c48d926ae5f5d2e66b1ea4dd41d458
ONBASE\cross <chris.ross@onbase.net>
2018-03-08 11:33:20 -0500

    Adding and removing skills works as expected

--- src/app/components/new-user/new-user.component.ts
+++ src/app/components/new-user/new-user.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PersonService } from '../shared/person.service';
 import { IPerson } from '../shared/person.model';
 
+import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
@@ -35,6 +37,15 @@ export class NewUserComponent implements OnInit {
     };
 
     this.personService.addPerson(person);
+  }
+
+  removeSkill(index: number) {
+    if (this.skills.length > 1) {
+      for (let i = index; i < this.skills.length; i++) {
+        this.skills[i] = this.skills[i + 1];
+      }
+    this.skills.length = this.skills.length - 1;
+    }
   }
 
   customTrackBy(index: number, obj: any): any {
